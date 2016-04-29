76cc8939d961d4696d74408b01e9e4040141acc6
Mark Hkr <markhker@gmail.com>
2016-04-28 19:59:23 -0500

    Update to Tag Manager

--- components/GoogleAnalytics/GoogleAnalytics.js
+++ components/GoogleAnalytics/GoogleAnalytics.js
@@ -8,12 +8,11 @@ import React from 'react';
 import { googleAnalyticsId } from '../../config';
 
 const trackingCode = { __html:
-  `(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=` +
-  `function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;` +
-  `e=o.createElement(i);r=o.getElementsByTagName(i)[0];` +
-  `e.src='https://www.google-analytics.com/analytics.js';` +
-  `r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));` +
-  `ga('create','${googleAnalyticsId}','auto');`,
+`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':` +
+`new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],` +
+`j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=` +
+`'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);` +
+`})(window,document,'script','dataLayer','${googleAnalyticsId}');`,
 };
 
 function GoogleAnalytics() {
