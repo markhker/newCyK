eb8b3eccf4249ab67d0a726e24208cda6cd28a27
Mark Hkr <markhker@gmail.com>
2016-05-02 17:57:11 -0500

    Created nav

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -1,19 +1,58 @@
-
 import React from 'react';
-import './Navigation.scss';
+import AppBar from 'material-ui/lib/app-bar';
+import LeftNav from 'material-ui/lib/left-nav';
+import MenuItem from 'material-ui/lib/menus/menu-item';
+//import IconButton from 'material-ui/lib/icon-button';
+//import NavigationMenu from 'material-ui/lib/svg-icons/navigation/menu';
+import FlatButton from 'material-ui/lib/flat-button';
 import Link from '../Link';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
-function Navigation() {
-  return (
-    <ul className="Navigation" role="menu">
-      <li className="Navigation-item">
-        <a className="Navigation-link" href="/" onClick={Link.handleClick}>Home</a>
-      </li>
-      <li className="Navigation-item">
-        <a className="Navigation-link" href="/about" onClick={Link.handleClick}>About</a>
-      </li>
-    </ul>
-  );
+injectTapEventPlugin();
+
+export default class Navigation extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {open: false};
+  }
+
+  handleToggle = () => this.setState({open: !this.state.open});
+
+  handleClose = () => this.setState({open: false});
+
+  render() {
+    return (
+      <div>
+        <AppBar
+            title="Cloud & Kloud"
+            //showMenuIconButton={false}
+            //iconElementRight={<IconButton><NavigationMenu /></IconButton>}
+            onLeftIconButtonTouchTap={this.handleToggle}
+          >
+          <FlatButton 
+            label="Servicios"
+            linkButton={true}
+            primary={true}
+            href="/servicios" 
+            onClick={Link.handleClick}/>
+          <FlatButton 
+            label="Nosotros"
+            linkButton={true}
+            primary={true}
+            href="/nosotros" 
+            onClick={Link.handleClick}/>
+          </AppBar>
+        <LeftNav
+          docked={false}
+          width={200}
+          open={this.state.open}
+          onRequestChange={open => this.setState({open})}
+        >
+          <MenuItem onTouchTap={this.handleClose}>Menu Item</MenuItem>
+          <MenuItem onTouchTap={this.handleClose}>Menu Item 2</MenuItem>
+        </LeftNav>
+      </div>
+    );
+  }
 }
-
-export default Navigation;
